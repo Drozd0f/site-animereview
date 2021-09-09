34a4e66ff7f9f69840ca329f15dfaf6315de6133
Amogus <600dan600@gmail.com>
2021-09-09 16:01:27 +0300

    Created url patterns

---
+++ animreview/reviews/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.ReviewsHome.as_view(), name='index'),
+    path('api/v1/temp', views.temp, name='temp'),
+    path('addpage/', views.AddPage.as_view(), name='add_post'),
+    path('user/update/<int:pk>', views.UserUpdateProfile.as_view(), name='profile_update'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
+    path('logout/', views.logout_user, name='logout'),
+    path('post/<slug:post_slug>/', views.ShowPost.as_view(), name='post'),
+    path('genre/<int:genre_id>/', views.PostGenre.as_view(), name='genre'),
+]
